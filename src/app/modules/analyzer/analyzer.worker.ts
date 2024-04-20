02291e1949676c98c9e9d8a82031c88d6a5e7d9e
Mueem-Nahid <mueem51@gmail.com>
2024-04-20 23:54:57 +0600

    # Info - update worker

--- src/app/modules/analyzer/analyzer.worker.ts
+++ src/app/modules/analyzer/analyzer.worker.ts
@@ -21,9 +21,17 @@ const analyzeChunk = (text: string, functionName: string) => {
 };
 
 let count = 0;
-let str;
+let str: unknown[];
 
+const promises = [];
 for (let i = 0; i < text.length; i += chunkSize) {
+  const chunk = text.substring(i, i + chunkSize);
+  promises.push(new Promise((resolve) => {
+    resolve(analyzeChunk(chunk, functionName));
+  }));
+}
+
+/*for (let i = 0; i < text.length; i += chunkSize) {
   const chunk = text.substring(i, i + chunkSize);
   if (functionName !== 'findLongestWords') {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -32,6 +40,20 @@ for (let i = 0; i < text.length; i += chunkSize) {
   } else {
     str = analyzeChunk(chunk, functionName);
   }
-}
+}*/
 
-parentPort?.postMessage(count ? count : str);
+Promise.all(promises)
+  .then((results) => {
+    if (functionName !== 'findLongestWords') {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      count = results.reduce((acc, cur) => acc + cur, 0); // Sum counts
+    } else {
+      str = results.flat();
+    }
+    parentPort?.postMessage(count ? count : str);
+  })
+  .catch((error) => {
+    parentPort?.postMessage({ error });
+  });
+
