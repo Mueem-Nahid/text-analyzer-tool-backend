fb4cd846cd3f2a3c2149d7b7a4c01069f352e0e3
Mueem-Nahid <mueem51@gmail.com>
2024-04-21 13:50:16 +0600

    # Fix - lint error

--- src/errors/handleValidationError.ts
+++ src/errors/handleValidationError.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
-import { IGenericErrorMessage } from '../interfaces/error';
 import { IGenericErrorResponse } from '../interfaces/common';
+import { IGenericErrorMessage } from '../interfaces/error';
 
 const handleValidationError = (
-  error: mongoose.Error.ValidatorError
+  error: mongoose.Error.ValidationError
 ): IGenericErrorResponse => {
   const errors: IGenericErrorMessage[] = Object.values(error.errors).map(
-    (el): IGenericErrorMessage => {
+    (el: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
       return {
         path: el?.path,
         message: el?.message,
@@ -16,7 +16,7 @@ const handleValidationError = (
   const statusCode = 400;
   return {
     statusCode,
-    message: 'Validation error',
+    message: 'Validation Error',
     errorMessages: errors,
   };
 };
