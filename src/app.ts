d5f15c206bb9396f55ad44ec718d42263453b8c4
Mueem-Nahid <mueem51@gmail.com>
2024-04-24 17:50:24 +0600

    # Info - clear unused middleware fn

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,11 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
 import httpStatus from 'http-status';
 import cookieParser from 'cookie-parser';
-import {keycloak, passport} from './helpers/keycloak';
-import session from 'express-session';
 import limiter from './app/middlewares/throttle';
 import morgan from "morgan";
 import winston from "winston";
-// import {keycloakAdminClient, keycloakAdminConfig} from "./helpers/keycloakAdmin";
-// import tokenRequester from "keycloak-request-token";
 
 const app: Application = express();
-
-const memoryStore = new session.MemoryStore();
 
 app.use(cors());
 
@@ -27,79 +21,6 @@ winston.add(new winston.transports.Console());
 
 // cookie parser
 app.use(cookieParser());
-
-// // Passport middleware
-app.use(session({
-  secret: 'mySecret',
-  resave: false,
-  saveUninitialized: true,
-  store: memoryStore,
-}));
-
-// Keycloak middleware
-app.use(keycloak.middleware());
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-/*app.use(async (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(' ')[1];
-    console.log("token here: ", token);
-    const settings = {
-      username: 'test-user',
-      password: 'password',
-      grantType: 'password',
-      clientId: keycloakAdminConfig.clientId,
-    };
-    if (token) {
-      /!*tokenRequester(keycloakAdminConfig.baseUrl, settings)
-        .then((token) => {
-          console.log(token, "token ==================");
-        }).catch((err) => {
-        console.log('err ======================', err);
-      });*!/
-      const userInfo = keycloakAdminClient.accessToken;
-      console.log("user info: ", userInfo);
-      // req.user = userInfo; // Attach user info to request object
-    }
-    next();
-  } catch (error) {
-    console.error('Keycloak middleware error:', error);
-    res.status(401).json({ message: 'Unauthorized' });
-  }
-});*/
-
-/*app.use(async (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(' ')[1];
-    console.log("token here: ", token);
-    if (token) {
-      const grantManager = await keycloakAdminClient.grantManager.create({
-        clientId: keycloakAdminConfig.clientId,
-        clientSecret: keycloakAdminConfig.clientSecret,
-      });
-
-      const grant = await grantManager.obtainDirectly({
-        grantType: 'password',
-        clientId: keycloakAdminConfig.clientId,
-        subject: 'test-user',
-        password: 'password',
-      });
-
-      const accessToken = grant.access_token; // Access token
-      // You can decode and verify the token here to get user info
-      console.log("user info: ", accessToken.content);
-      next();
-    } else {
-      next();
-    }
-  } catch (error) {
-    console.error('Keycloak middleware error:', error);
-    res.status(401).json({ message: 'Unauthorized' });
-  }
-});*/
 
 app.use(limiter);
 
